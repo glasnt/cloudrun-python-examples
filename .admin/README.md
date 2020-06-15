f9f905339e40a6b672dbae5feb98b84fe2de11ab
Katie McLaughlin <katie@glasnt.com>
2020-06-15 17:35:45 +1000

    Add base admin service

---
+++ .admin/README.md
@@ -0,0 +1,27 @@
+# Repo admin
+
+All examples are automatically deployed. 
+
+## Admin Service
+
+This service shows the real-time deployment status of the examples, without having to change each example to self-report. 
+
+## Service Deployment
+
+Triggers are setup so that when the folder is updated in the latest branch, the service re-deploys.
+
+Bulk trigger creation script: 
+
+```
+for d in */ ; do
+SERVICE=${d%/}
+gcloud beta builds triggers create github \
+  --repo-owner glasnt \
+  --repo-name cloudrun-python-examples \
+  --branch-pattern latest \
+  --build-config cloudbuild.yaml \
+  --included-files "${SERVICE}/**" \
+  --description ${SERVICE} \
+  --substitutions _SERVICE=${SERVICE}
+done
+```
---
+++ .admin/app.py
@@ -0,0 +1,66 @@
+import os
+import json
+
+from flask import Flask, jsonify
+
+import google.auth
+from googleapiclient.discovery import build
+
+app = Flask(__name__)
+
+_, project = google.auth.default()
+region = os.environ.get("REGION", "us-central1")
+
+
+@app.route("/report")
+def report():
+    run = build("run", "v1")
+    services = (
+        run.projects()
+        .locations()
+        .services()
+        .list(parent=f"projects/{project}/locations/{region}")
+        .execute()
+    )
+
+    cb = build("cloudbuild", "v1")
+    builds = cb.projects().builds().list(projectId=project).execute()
+    triggers = cb.projects().triggers().list(projectId=project).execute()
+
+    results = {}
+
+    for srv in services["items"]:
+        name = srv["metadata"]["name"]
+
+        if name == "admin":
+            continue
+
+        trigger = [
+            t for t in triggers["triggers"] if t["substitutions"]["_SERVICE"] == name
+        ]
+        if trigger:
+            trigger = trigger[0]
+
+            last_build = [
+                b
+                for b in builds["builds"]
+                if "buildTriggerId" in b and b["buildTriggerId"] == trigger["id"]
+            ][0]
+            commit_id = last_build["substitutions"]["SHORT_SHA"]
+            finishTime = last_build["finishTime"]
+        else:
+            trigger = None
+            commit_id = None
+            finishTime = None
+
+        results[name] = {"commit_id": commit_id, "finishTime": finishTime}
+    return jsonify(results)
+
+
+@app.route("/")
+def home():
+    return "Run /report (may take a while)"
+
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=8080, debug=True)

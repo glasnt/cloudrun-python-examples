8d58d1b9c35535596c2f5f5eb849e013e66a0e39
Katie McLaughlin <katie@glasnt.com>
2020-06-17 18:25:24 +1000

    Cleanup admin logic, with filters!

--- .admin/app.py
+++ .admin/app.py
@@ -25,56 +25,104 @@ def run_report():
     )
 
     cb = build("cloudbuild", "v1")
-    builds = cb.projects().builds().list(projectId=project).execute()
-    triggers = cb.projects().triggers().list(projectId=project).execute()
 
     results = {}
-    # TODO(glasnt) cleanup
     for srv in services["items"]:
         name = srv["metadata"]["name"]
         url = srv["status"]["url"]
 
+        trigger = None; commit_id = None; finishTime = None; success = None
+
         if name == "admin":
             continue
 
-        trigger = [
-            t for t in triggers["triggers"]
-                if "substitutions" in t.keys()
-                    and "_SERVICE" in t["substitutions"].keys()
-                    and t["substitutions"]["_SERVICE"] == name
-        ]
-        if trigger:
-            trigger = trigger[0]
-
-            last_build = [
-                b
-                for b in builds["builds"]
-                if "buildTriggerId" in b and b["buildTriggerId"] == trigger["id"]
-                and "finishTime" in b.keys()
-            ]
+        trigger_id = get_trigger_id(cb, name)
+        if trigger_id:
+            last_build = latest_build(cb, trigger_id)
             if last_build:
-                commit_id = last_build[0]["substitutions"]["SHORT_SHA"]
-                finishTime = last_build[0]["finishTime"]
-                success = last_build[0]["status"] == "SUCCESS"
-            else:
-                trigger = None
-                commit_id = None
-                finishTime = None
-                success = None
-        else:
-            trigger = None
-            commit_id = None
-            finishTime = None
-            success = None
+                commit_id = last_build["substitutions"]["SHORT_SHA"]
+                finishTime = last_build["finishTime"]
+                success = last_build["status"] == "SUCCESS"
 
-        results[name] = {"commit_id": commit_id, "finishTime": finishTime,
-                "url": url, "success": success}
+        results[name] = {"commit_id": commit_id,
+                        "finishTime": finishTime,
+                         "url": url,
+                         "success": success}
     return results
+
+def get_trigger_id(cb, name):
+    triggers = cb.projects().triggers().list(projectId=project).execute()
+    for t in triggers["triggers"]:
+        if "substitutions" not in t.keys():
+            continue
+        if "_SERVICE" not in t["substitutions"].keys():
+            continue
+        if t["substitutions"]["_SERVICE"] == name:
+            return t["id"]
+    return None
+
+
+
+def latest_build(cb, trigger_id):
+    kwargs = {
+                "projectId": project,
+                "filter": f'trigger_id="{trigger_id}"',
+                "pageSize": 1
+            }
+    # presumes order
+    builds = cb.projects().builds().list(**kwargs).execute()
+    if builds:
+        return builds["builds"][0]
+    else:
+        return None
 
 
 @app.route("/api")
 def report():
     return jsonify(run_report())
+
+def relative_time(dstr):
+    if not dstr:
+        return "(no data)"
+    delta = dateparser.parse("now Z") - dateparser.parse(dstr, settings={"TIMEZONE": "Z"})
+    return f"{format_timedelta(delta)} ago"
+
+@app.route("/status")
+def status():
+    data = run_report()
+
+    # converting dict to list, for table formatting ease
+    resp = []
+    for key in data.keys():
+        state = data[key]
+        state["name"] = key
+        state["relativeFinishTime"] = relative_time(state["finishTime"])
+        resp.append(state)
+    return render_template("index.html", data=resp)
+
+@app.route("/status/<service>.svg")
+def status_image(service):
+    cb = build("cloudbuild", "v1")
+    commit_id = "unknown"; success_color = "inactive"
+    trigger_id = get_trigger_id(cb, service)
+    if trigger_id:
+        result = latest_build(cb, trigger_id)
+        if result:
+            if result["status"] == "SUCCESS":
+                success_color = "success"
+            else:
+                success_color = "critical"
+            if "substitutions" in result.keys() and "SHORT_SHA" in result["substitutions"].keys():
+                commit_id = result["substitutions"]["SHORT_SHA"]
+            else:
+                commit_id = "unknown"
+
+    shield_label = "Last sample deployed at commit".replace(" ", "%20")
+    shield_url = (f"https://img.shields.io/badge/{shield_label}-{commit_id}-{success_color}"
+                   "?style=flat-square&logo=google-cloud&logoColor=white")
+    resp = httpx.get(shield_url)
+    return Response(resp.content, mimetype="image/svg+xml")
+
 
 
 @app.route("/")
@@ -92,47 +140,6 @@ def home():
         xhr.send();
     </script>
     """
-def relative_time(dstr):
-    if not dstr:
-        return "(no data)"
-    delta = dateparser.parse("now Z") - dateparser.parse(dstr, settings={"TIMEZONE": "Z"})
-    return f"{format_timedelta(delta)} ago"
-
-@app.route("/status")
-def status():
-    data = run_report()
-    resp = []
-    for key in data.keys():
-        state = data[key]
-        state["name"] = key
-        state["relativeFinishTime"] = relative_time(state["finishTime"])
-        resp.append(state)
-    return render_template("index.html", data=resp)
-
-@app.route("/status/<service>.svg")
-def status_image(service):
-    data = run_report() # TODO(glasnt) - do a more efficient call
-    if service in data.keys():
-        result = data[service]
-
-        if result["success"]:
-            success_color = "success"
-        else:
-            success_color = "critical"
-        commit_id = result["commit_id"]
-    else:
-        commit_id = "unknown"
-        success_color = "inactive"
-
-    shield_label = "Last sample deployed at commit".replace(" ", "%20")
-    shield_url = (f"https://img.shields.io/badge/{shield_label}-{commit_id}-{success_color}"
-                   "?style=flat-square&logo=google-cloud&logoColor=white")
-    resp = httpx.get(shield_url)
-    return Response(resp.content, mimetype="image/svg+xml")
-
-@app.route("/status")
-def status_home():
-    return str(request.__dict__)
 
 
 if __name__ == "__main__":
